e9f94b11c489c69fff9d706f8389594a05dee341
Amy Gori <amydgori@gmail.com>
2021-11-05 11:31:44 -0400

    Add note detail url, view, and template

--- contacts/views.py
+++ contacts/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Contact
-from .forms import ContactForm
+from .forms import ContactForm, NoteForm
 
 
 # Create your views here.
 def list_contacts(request):
     contacts = Contact.objects.all()
     return render(request, "contacts/list_contacts.html", {"contacts": contacts})
+
+
+def contact_detail(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    form = NoteForm()
+    return render(
+        request,
+        "contacts/contact_detail.html",
+        {"contact": contact, "pk": pk, "form": form},
+    )
 
 
 def add_contact(request):
